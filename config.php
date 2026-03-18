9af4d5dd7bd4b3543829452d8001949125ac0f78
Sekki011604 <202280146@psu.palawan.edu.ph>
2026-03-18 09:39:43 +0800

    ignore

--- config.php
+++ config.php
@@ -6,7 +6,7 @@
 define('DB_HOST',    '127.0.0.1');
 define('DB_NAME',    'pho_budgeting');
 define('DB_USER',    'root');
-define('DB_PASS',    'root');
+define('DB_PASS',    '');
 define('DB_CHARSET', 'utf8mb4');
 
 define('APP_NAME', '2026 Conso Proposal V3');
